9c8d1bec5f3cac15b12b921b9e6c8e42f6766ed3
aronday <aron.day@datadoghq.com>
2024-05-05 15:52:17 +0100

    Updated

--- datadog.tf
+++ datadog.tf
@@ -1,6 +1,26 @@
 # AWS Config for Datadog AWS Integration
 # https://docs.datadoghq.com/integrations/guide/aws-terraform-setup/
 
+resource "aws_secretsmanager_secret" "dd_api_key" {
+  name        = "datadog_api_key"
+  description = "Encrypted Datadog API Key"
+}
+
+resource "aws_secretsmanager_secret_version" "dd_api_key" {
+  secret_id     = aws_secretsmanager_secret.dd_api_key.id
+  secret_string = var.datadog_api_key
+}
+
+resource "aws_secretsmanager_secret" "dd_app_key" {
+  name        = "datadog_app_key"
+  description = "Encrypted Datadog APP Key"
+}
+
+resource "aws_secretsmanager_secret_version" "dd_app_key" {
+  secret_id     = aws_secretsmanager_secret.dd_app_key.id
+  secret_string = var.datadog_app_key
+}
+
 data "aws_iam_policy_document" "datadog_aws_integration_assume_role" {
   depends_on = [datadog_integration_aws.aws_integration]
   statement {
--- ec2.tf
+++ ec2.tf
@@ -3,6 +3,13 @@
 resource "aws_instance" "sample_server" {
   ami           = "ami-07edc58546d708802"
   instance_type = "t2.micro"
+  user_data = <<-EOF
+              #!/bin/bash
+              yum update -y
+              yum install -y aws-cli jq curl
+              DD_API_KEY=$(aws secretsmanager get-secret-value --secret-id datadog_api_key)
+              DD_SITE="datadoghq.com"  bash -c "$(curl -L https://s3.amazonaws.com/dd-agent/scripts/install_script_agent7.sh)"
+              EOF
 
   tags = {
     Name = "Tines Sample EC2 Instance"
